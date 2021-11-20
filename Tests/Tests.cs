924b54b9a161eb584e6a6f72960aefd7a036d722
g0dzZz1lla <g0dzZz1lla@yandex.ru>
2021-11-21 01:17:07 +0300

    + Some tests

---
+++ Tests/Tests.cs
@@ -0,0 +1,53 @@
+using Depra.EventManager.Core;
+using Depra.EventManager.Core.Dispose;
+using NUnit.Framework;
+
+public class Tests
+{
+    private readonly EventDisposal _disposal = new EventDisposal();
+    
+    [Test]
+    public void Single_Event_Successfully_Invoked()
+    {
+        var flag = false;
+        EventManager.Add("Single", () =>
+        {
+            flag = true;
+        }).AddTo(_disposal);
+        
+        EventManager.Invoke("Single");
+        _disposal.Dispose();
+        
+        Assert.IsTrue(flag);
+    }
+
+    [Test]
+    public void Object_Event_Successfully_Invoked()
+    {
+        var flag = false;
+        EventManager.Add("Objects", objects =>
+        {
+            flag = true;
+        }).AddTo(_disposal);;
+        
+        EventManager.Invoke("Objects");
+        _disposal.Dispose();
+        
+        Assert.IsTrue(flag);
+    }
+
+    [Test]
+    public void Dynamic_Event_Successfully_Invoked()
+    {
+        var flag = false;
+        DynamicEventManager.Add("Dynamic", dynamic =>
+        {
+            flag = true;
+        }).AddTo(_disposal);
+        
+        DynamicEventManager.Invoke("Dynamic");
+        _disposal.Dispose();
+        
+        Assert.IsTrue(flag);
+    }
+}
